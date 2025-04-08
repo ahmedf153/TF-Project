b98cd5e2d4524b5daaee8ad9e27c6622218c735a
ahmedf153 <faraga389@gmail.com>
2025-04-08 16:26:17 +0200

    Update variables.tf

--- tf-project/variables.tf
+++ tf-project/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "us-east-1"  # Change this to your actual region if needed
+  default     = "us-east-1"  
 }
